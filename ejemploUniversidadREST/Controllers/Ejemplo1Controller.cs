b2a832ff61f586ac0003dca16e72a1851f13403b
WinTux <rusok.software@gmail.com>
2025-07-22 20:06:08 -0400

    Agregar archivos de proyecto.

---
+++ ejemploUniversidadREST/Controllers/Ejemplo1Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ejemploUniversidadREST.Controllers
+{
+    [Route("api/Ejemplo1")] // http://localhost:5128/api/ejemplo1
+    public class Ejemplo1Controller : ControllerBase
+    {
+        [HttpGet("prueba1")] // http://localhost:5128/api/ejemplo1/prueba1
+        public async Task<IActionResult> prueba1()
+        {
+            try
+            {
+                return Ok("Prueba 1 exitosa");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+    }
+}
